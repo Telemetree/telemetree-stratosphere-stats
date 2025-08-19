6546fbd6ceb64762e375ee2a45dcde03fc351001
Chris Cherniakov <taequn@gmail.com>
2025-08-18 21:05:25 -0700

    feat(notion-utils): add function to parse timeseries data for expect Notion format

--- src/notion/notion_utils.py
+++ src/notion/notion_utils.py
@@ -1,6 +1,12 @@
 from typing import Any
 
 from src.notion.notion_constants import (
+    CHANNEL_TIMESERIES_DATE_ID,
+    CHANNEL_TIMESERIES_HANDLE_ID,
+    CHANNEL_TIMESERIES_JOINED_ID,
+    CHANNEL_TIMESERIES_LEFT_ID,
+    CHANNEL_TIMESERIES_MUTE_ID,
+    CHANNEL_TIMESERIES_TOTAL_FOLLOWERS_ID,
     STATE_DATE_ID,
     STATE_FOLLOWERS_ID,
     STATE_HANDLE_ID,
@@ -69,3 +75,63 @@ def process_state_data(
     }
 
     return state_data
+
+
+def process_timeseries_data(
+    date: str, handle: str, joined: int, mute: int, left: int, followers: int
+) -> dict[str, Any]:
+    """
+    Process the channel timeseries data and transform into Notion expected format.
+    """
+    assert joined is not None and joined >= 0, "Joined must be a positive number"
+    assert mute is not None and mute >= 0, "Mute must be a positive number"
+    assert left is not None and left >= 0, "Left must be a positive number"
+    assert followers is not None and followers >= 0, (
+        "Followers must be a positive number"
+    )
+
+    assert isinstance(date, str), "Date must be a string"
+    assert isinstance(handle, str), "Handle must be a string"
+
+    assert date is not None and handle is not None, "Date and handle must be provided"
+    timeseries_data = {
+        "Joined": {
+            "id": CHANNEL_TIMESERIES_JOINED_ID,
+            "type": "number",
+            "number": joined,
+        },
+        "Date": {
+            "id": CHANNEL_TIMESERIES_DATE_ID,
+            "type": "date",
+            "date": {"start": date, "end": None, "time_zone": None},
+        },
+        "Mute": {"id": CHANNEL_TIMESERIES_MUTE_ID, "type": "number", "number": mute},
+        "Left": {"id": CHANNEL_TIMESERIES_LEFT_ID, "type": "number", "number": left},
+        "Total followers": {
+            "id": CHANNEL_TIMESERIES_TOTAL_FOLLOWERS_ID,
+            "type": "number",
+            "number": followers,
+        },
+        "Handle": {
+            "id": CHANNEL_TIMESERIES_HANDLE_ID,
+            "type": "title",
+            "title": [
+                {
+                    "type": "text",
+                    "text": {"content": handle, "link": None},
+                    "annotations": {
+                        "bold": False,
+                        "italic": False,
+                        "strikethrough": False,
+                        "underline": False,
+                        "code": False,
+                        "color": "default",
+                    },
+                    "plain_text": handle,
+                    "href": None,
+                }
+            ],
+        },
+    }
+
+    return timeseries_data
